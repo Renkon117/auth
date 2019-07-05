199d8460e28758a567f878210f31e0fd355e9d57
okada001 <okada001@rangers.uwp.edu>
2019-07-05 18:14:03 +0900

    connected to DB, added mongoose

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
+  console.log("connected to DB")
+);
 
 //import Routes
 const authRoute = require("./routes/auth");
 
 //Route Middleware
-app.use("./api/user", authRoute);
+app.use("/api/user", authRoute);
 
 app.listen(4000, () => console.log("Server up and running"));
