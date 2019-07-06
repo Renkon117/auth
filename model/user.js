3c1162c12261fb6ac4f9692dc5f1cee32035efd7
okada001 <okada001@rangers.uwp.edu>
2019-07-06 13:24:55 +0900

    created register & login validation

--- model/user.js
+++ model/user.js
@@ -3,19 +3,19 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: true,
+    required: true,
     max: 255,
     min: 6
   },
   email: {
     type: String,
-    require: true,
+    required: true,
     max: 255,
     min: 6
   },
   password: {
     type: String,
-    require: true,
+    required: true,
     max: 1024,
     min: 6
   },
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
 const User = require("../model/User");
 
+const { registerValidation, loginValidation } = require("../validation");
+
 router.post("/register", async (req, res) => {
+  //LETS VALIDATE THE DATA BEFORE WE A USER
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //Checking if the user is already in the database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exists");
+
+  //CREATE A NEW USER
   const user = new User({
     name: req.body.name,
     email: req.body.email,
